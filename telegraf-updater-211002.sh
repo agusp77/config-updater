63559b1c12e48931e2929ece7d8a743a59767e3a
Agus P <agusp@gt-pro.live>
2021-10-02 16:54:02 +1000

    Update

---
+++ telegraf-updater-211002.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# CONFIG UPDATER
+# TELEGRAF
+# Turn the influxdb url from http to https
+#
+
+PATH="/etc/telegraf/telegraf.conf"
+
+old_text1="http://"
+new_text1="https://"
+add_text="insecure_skip_verify = true"
+
+sed -i 's/$old_text1/$new_text1' $PATH
+echo $add_text >> $PATH
+
+echo "--------------------------------------------------------------------------"
+cat $PATH
+echo "--------------------------------------------------------------------------"
+echo "Finished Updating"
+
+
