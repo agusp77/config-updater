b3ebece6b6be15916a5ca05103ea6675c94412af
Agus P <agusp@gt-pro.live>
2021-10-02 19:29:18 +1000

    Update

--- telegraf-updater-211002.sh
+++ telegraf-updater-211002.sh
@@ -14,8 +14,8 @@ old_text1="http\:\/\/139.59.251.65\:8086"
 new_text1="https\:\/\/139.59.251.65\:8086"
 add_text1="insecure_skip_verify = true"
 
-#sed -i 's/$old_text1/$new_text1/g' $TARGET
-sed -i 's/http\:\/\/139.59.251.65\:8086/https\:\/\/139.59.251.65\:8086/g' 
+sed -i "s/${old_text1}/${new_text1}/g" $TARGET
+# sed -i 's/http\:\/\/139.59.251.65\:8086/https\:\/\/139.59.251.65\:8086/g' 
 
 echo $add_text1 >> $TARGET
 
