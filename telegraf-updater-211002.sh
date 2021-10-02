a5deeca1665aa94b07f9ba3dbf19184dd0c36ce3
Agus P <agusp@gt-pro.live>
2021-10-02 16:58:27 +1000

    Update

--- telegraf-updater-211002.sh
+++ telegraf-updater-211002.sh
@@ -11,7 +11,7 @@ old_text1="http://"
 new_text1="https://"
 add_text="insecure_skip_verify = true"
 
-sed -i 's/$old_text1/$new_text1' $PATH
+sed -i 's/$old_text1/$new_text1/g' $PATH
 echo $add_text >> $PATH
 
 echo "--------------------------------------------------------------------------"
