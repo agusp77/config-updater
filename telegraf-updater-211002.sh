2d3e8b5f7abff45d22a731a7583970ee0ce3b3ba
Agus P <agusp@gt-pro.live>
2021-10-02 17:05:07 +1000

    Update

--- telegraf-updater-211002.sh
+++ telegraf-updater-211002.sh
@@ -9,10 +9,11 @@ TARGET="/etc/telegraf/telegraf.conf"
 
 old_text1="http://"
 new_text1="https://"
-add_text="insecure_skip_verify = true"
+add_text1="insecure_skip_verify = true"
 
 sed -i 's/$old_text1/$new_text1/g' $TARGET
-echo $add_text >> $TARGET
+
+echo $add_text1 >> $TARGET
 
 systemctl restart telegraf
 
